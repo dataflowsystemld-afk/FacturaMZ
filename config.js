4a3f9637e16737a9113082a71decab8f31c291f5
dataflowsystemld-afk <dataflowsystemld@gmail.com>
2026-06-04 12:20:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // ══════════════════════════════════════════════════════
 
 const CONFIG = {
-  SUPABASE_URL: 'https://uzuxzbzvxhnhsoqfnqcw.supabase.co/rest/v1/',   // ← Ex: https://xyzabc.supabase.co
+  SUPABASE_URL: 'https://uzuxzbzvxhnhsoqfnqcw.supabase.co',   // ← Ex: https://xyzabc.supabase.co
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dXh6Ynp2eGhuaHNvcWZucWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTI2MzEsImV4cCI6MjA5NjEyODYzMX0.EcwHamFMoTbgj8ABu8eGIBZ1YXJ-YJChZNYa2H0hZ4w'    // ← anon/public key (começa com "eyJ...")
 };
